8054e463d648b4e23bf43cb3432647428bf32271
Mika-Tweak <mikhail.saliba99@gmail.com>
2024-06-19 00:26:43 -0700

    got rid of a whoopsie

--- utils/supabase.js
+++ utils/supabase.js
@@ -5,8 +5,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-console.log('Supabase URL:', supabaseUrl)
-console.log('Supabase Key:', supabaseKey)
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Please define NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables inside .env.local file")
